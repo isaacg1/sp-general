25c3d4a3a958e507dfc050e62b47a735444e0a49
Isaac Grosof <igrosof@cmu.edu>
2017-10-06 13:29:10 -0400

    Interchange style

--- src/main.rs
+++ src/main.rs
@@ -82,14 +82,9 @@ impl Layout {
             succs.push(new);
         }
         if can_place_parallel {
-            for width in (1..self.processors + 1).rev() {
-                let mut new = self.clone();
-                let res = new.try_place_parallel(width);
-                if res.is_ok() {
-                    succs.push(new);
-                    break;
-                }
-            }
+            let mut new = self.clone();
+            new.place_parallel();
+            succs.push(new);
         }
         succs
     }
@@ -131,6 +126,15 @@ impl Layout {
             self.profile[index] = new_height;
         }
         Ok(())
+    }
+
+    fn place_parallel(&mut self) {
+        for width in (1..self.processors + 1).rev() {
+            let res = self.try_place_parallel(width);
+            if res.is_ok() {
+                break;
+            }
+        }
     }
 
     fn dominates(&self, other: &Self) -> bool {
@@ -204,37 +208,95 @@ fn opt_layout(processors: usize, serials: &Vec<f64>, parallels: &Vec<f64>) -> La
 fn srpt_layout(processors: usize, serials: &Vec<f64>, parallels: &Vec<f64>) -> Layout {
     let mut layout = Layout::new(processors, serials.clone(), parallels.clone());
     for _ in 0..serials.len() + parallels.len() {
-        if layout.next_serial_index < layout.serials.len() &&
-            layout.serials[layout.next_serial_index] <
-                layout.parallels[layout.next_parallel_index]
+        if layout.next_parallel_index >= layout.parallels.len() ||
+            layout.next_serial_index < layout.serials.len() &&
+                layout.serials[layout.next_serial_index] <
+                    layout.parallels[layout.next_parallel_index]
         {
             layout.place_serial();
         } else {
-            for width in (1..processors + 1).rev() {
-                let res = layout.try_place_parallel(width);
-                if res.is_ok() {
-                    break;
-                }
-            }
+            layout.place_parallel();
         }
     }
     layout
 }
 
+fn ordering_to_layout(
+    ordering: &Vec<(usize, bool)>,
+    processors: usize,
+    serials: &Vec<f64>,
+    parallels: &Vec<f64>,
+) -> Layout {
+    let mut layout = Layout::new(processors, serials.clone(), parallels.clone());
+    for &(_index, is_parallel) in ordering {
+        if is_parallel {
+            layout.place_parallel();
+        } else {
+            layout.place_serial();
+        }
+    }
+    layout
+}
+
+fn interchange_layout(processors: usize, serials: &Vec<f64>, parallels: &Vec<f64>) -> Layout {
+    let mut ordering: Vec<(usize, bool)> = (0..serials.len())
+        .map(|i| (i, false))
+        .chain((0..parallels.len()).map(|i| (i, true)))
+        .collect();
+    ordering.sort_by_key(|&(i, is_parallel)| {
+        Wrap::Val(if is_parallel {
+            parallels[i]
+        } else {
+            serials[i]
+        })
+    });
+    let mut changed = true;
+    let mut current = ordering_to_layout(&ordering, processors, serials, parallels);
+    while changed {
+        changed = false;
+        current = ordering_to_layout(&ordering, processors, serials, parallels);
+        let mut new_ordering = ordering.clone();
+        for (start_index, window) in ordering.windows(2).enumerate() {
+            let prev = window[0];
+            let next = window[1];
+
+            if prev.1 != next.1 {
+                new_ordering = ordering.clone();
+                new_ordering[start_index] = next;
+                new_ordering[start_index + 1] = prev;
+                let new_layout = ordering_to_layout(&new_ordering, processors, serials, parallels);
+                if new_layout.cost < current.cost {
+                    changed = true;
+                    println!("Swapped {:?} with {:?}", prev, next);
+                    break;
+                }
+            }
+        }
+        if changed {
+            ordering = new_ordering;
+        }
+    }
+    current
+}
+
 fn main() {
-    let serials = (0..40).map(|i|(2*i+1) as f64).collect();
-    let parallels = (0..40).map(|i|(2*i+2) as f64).collect();
-    let processors = 6;
+    let serials = vec![2., 4., 6., 8.];
+    let parallels = vec![1., 3., 5., 7.];
+    let processors = 4;
     let opt_layout = opt_layout(processors, &serials, &parallels);
     println!("Placements: {:?}", opt_layout.placements);
     println!("Opt cost: {}", opt_layout.cost);
     let srpt_out = srpt_layout(processors, &serials, &parallels);
     //println!("Placements: {:?}", srpt_out.placements);
     println!("SRPT cost: {}", srpt_out.cost);
-    let mut magic_parallels = serials.clone();
-    magic_parallels.extend(parallels);
-    let magic = srpt_layout(processors, &vec![], &magic_parallels);
-    //println!("Placements: {:?}", magic.placements);
-    println!("Magic cost: {}", magic.cost);
-
+    let interchange = interchange_layout(processors, &serials, &parallels);
+    println!("Interchange cost: {}", interchange.cost);
+    println!(
+        "SRPT-OPT/(sum serial): {}",
+        (srpt_out.cost - opt_layout.cost) / (serials.iter().sum::<f64>())
+    );
+    println!(
+        "SRPT-OPT/(sum parallel): {}",
+        (srpt_out.cost - opt_layout.cost) / (parallels.iter().sum::<f64>())
+    );
 }
